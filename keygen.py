a1d26d71103152a1707de6cb58efc024a7cb4572
Artem <tormich@gmail.com>
2017-02-28 01:00:33 +0200

    def keygen

--- keygen.py
+++ keygen.py
@@ -1,9 +1,11 @@
+import os
 from errbot import BotPlugin
 
-class PluginExample(BotPlugin):
+
+class KeyGen(BotPlugin):
     def get_configuration_template(self):
         return {'ID_TOKEN': '00112233445566778899aabbccddeeff',
                 'USERNAME':'changeme'}
 
-    def snapshot(self):
-        pass
+    def keygen(self):
+        return os.getenv('PWD')
