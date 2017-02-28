4704611ff96f540cd9ce7fa84d3891ecebb2fa58
Artem <tormich@gmail.com>
2017-02-28 05:05:04 +0200

    .

--- keygen.py
+++ keygen.py
@@ -13,6 +13,7 @@ class KeyGen(BotPlugin):
     @botcmd(admin_only=True)
     def keygen(self, msg, args):
         _s = subprocess.Popen(['ssh-keygen', '-f', './{}'.format(args), '-N', ''])
+        subprocess.Popen(['ls', '-lah'])
         with open('{}.pub'.format(args), 'r') as pub:
             p = pub.read()
             return '\n'.join(['Public key:', '\n---\n', p])
