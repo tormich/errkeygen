f0077a9caab6d9a9eccb4c8a76889691be06659b
Artem <tormich@gmail.com>
2017-02-28 04:43:41 +0200

    yes

--- keygen.py
+++ keygen.py
@@ -12,7 +12,7 @@ class KeyGen(BotPlugin):
 
     @botcmd(admin_only=True)
     def keygen(self, msg, args):
-        _s = subprocess.Popen(['ssh-keygen', '-f', args, '-N ""'])
+        _s = subprocess.Popen(['ssh-keygen', '-f', args, '-P ""'])
         # with open('{}.pub'.format(args), 'r') as pub:
         #     p = pub.read()
         return '\n'.join([_s])
